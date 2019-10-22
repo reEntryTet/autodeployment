ee9289b7775579adb179d77feb59caa1b5c799dc
fraaz.alam <fraaz.alam@kmgin.com>
2019-10-22 05:42:28 -0400

    test this

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 describe('AppComponent', () => {
+  const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AboutComponent
       ],
     }).compileComponents();
   }));
@@ -20,16 +30,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test'`, () => {
+  it(`should have as text 'Angular Unit Testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test');
+    expect(app.text).toEqual('Angular Unit Testing');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render text in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Unit Testing!');
   });
 });
